395a978c695965fa0a8ce40bda76e85f5e8ee0b9
CJ <90126284+CeeJank@users.noreply.github.com>
2025-05-31 18:38:56 +0800

    initial list setup for currencies

--- bot/telegram_bot.py
+++ bot/telegram_bot.py
@@ -5,6 +5,10 @@ from dotenv import load_dotenv
 import logging
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import filters, Application, ContextTypes, CommandHandler, MessageHandler, CallbackQueryHandler
+from exchangerate.exchange_rate import commonCurrencies
+from exchangerate.exchange_rate import moreCurrencies
+
+
 
 # logging
 logging.basicConfig(
@@ -16,20 +20,16 @@ logging.basicConfig(
 load_dotenv()
 token = os.environ.get("TG_TOKEN_API")
 
-
-
 # inline keyboard markups
 async def rates(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     # 2D-matrix 
     keyboard = [
-        [
             # row 1
-            InlineKeyboardButton("USD", callback_data="1"),
-            InlineKeyboardButton("SGD", callback_data="2"),
-        ],
-        # row 2
-        [InlineKeyboardButton("MYR", callback_data="3")],
+            [InlineKeyboardButton(currency, callback_data=f"{currency}") for currency in commonCurrencies()],
+            #row 2
+            [InlineKeyboardButton("More Currencies", callback_data="more")]
     ]
+
     # turns list into actual inlinekeyboard
     reply_markup = InlineKeyboardMarkup(keyboard)
 
@@ -37,24 +37,44 @@ async def rates(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text("Please Choose", reply_markup=reply_markup)
 
 
-
-# actual buttons of the keyboard
+# buttons handler of the keyboard
 async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     # access to the provided CallbackQuery
     query = update.callback_query
 
-    # answers the callbackquery
+    # acknowledges the button press
     await query.answer()
 
-    # edits the message where CallbackQuery comes from, then inserts data defined in keyboard(1,2,3)
-    # doesn't pass the inline keyboard again
-    await query.edit_message_text(text=f"You have chosen {query.data}")
+    data = query.data
+
+    # edits the message where CallbackQuery comes from, then inserts data defined in keyboard
+    # pass the inline keyboard again only with reply_markup
+    await query.edit_message_text(text=f"You have chosen {query.data}", reply_markup=InlineKeyboardMarkup([InlineKeyboardButton("rates", callback_data="rates")]))
+
+    if data == "more":
+        await more(update, context)
+    elif data == "reset":
+        await reset(update, context)
+
+
+async def more(update: Update, context:ContextTypes.DEFAULT_TYPE) -> None:
+
+    keyboard = [
+        [InlineKeyboardMarkup(callback_data=f"{more}")] for more in moreCurrencies()
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    await update.callback_query.edit_message_text
+
+
+async def reset() -> None: 
+    return
+
+
 
 # help command
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text("Use /rates to initialize")
-
-
 
 # initialize bot
 def buildBot():
--- exchangerate/exchange_rate.py
+++ exchangerate/exchange_rate.py
@@ -6,10 +6,29 @@ load_dotenv()
 
 ratetoken = os.environ.get("EXCHANGERATE_API")
 
+def pairCur(cur1, cur2):
+    url = f'https://v6.exchangerate-api.com/v6/{ratetoken}/pair/{cur1}/{cur2}'
+    response = requests.get(url)
+    data = response.json()
+    exchangeRate = [*data["conversion_rates"].values()]
+    return exchangeRate
+
 def getRates():
     url = f'https://v6.exchangerate-api.com/v6/{ratetoken}/latest/USD'
     response = requests.get(url)
-    data = response.json()
-    print(data)
+    currency_sheet = response.json()
+    moreCurrencies(currency_sheet)
+
+
+# generate most commonly used currencies
+def commonCurrencies():
+    currency = ["USD", "EUR", "JPY", "GBP", "AUD"]
+    return currency
+
+def moreCurrencies(dict):
+    more = [*dict["conversion_rates"].keys()]
+    return more
+
+
 
 
